3242aaeb84aefacc0c9b617dd04b8bf0df51c340
Trevor Huisintveld <trevorhuisintveld@Trevors-Mac-mini.local>
2025-08-25 23:13:02 -0500

    sync

--- fe-masters/completeIntroToReactV9/project/src/routes/past.lazy.jsx
+++ fe-masters/completeIntroToReactV9/project/src/routes/past.lazy.jsx
@@ -14,10 +14,42 @@ function PastOrdersRoute() {
     queryFn: () => getPastOrders(page),
     staleTime: 30000,
   });
-
   if (isLoading) {
-    return <div className="past-orders">...loading</div>;
+    return (
+      <div className="past-orders">
+        <h2>LOADING …</h2>
+      </div>
+    );
   }
-
-  return <div>Hello World!</div>;
+  return (
+    <div className="past-orders">
+      <table>
+        <thead>
+          <tr>
+            <td>ID</td>
+            <td>Date</td>
+            <td>Time</td>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((order) => (
+            <tr key={order.order_id}>
+              <td>{order.order_id}</td>
+              <td>{order.date}</td>
+              <td>{order.time}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <div className="pages">
+        <button disabled={page <= 1} onClick={() => setPage(page - 1)}>
+          Previous
+        </button>
+        <div>{page}</div>
+        <button disabled={data.length < 10} onClick={() => setPage(page + 1)}>
+          Next
+        </button>
+      </div>
+    </div>
+  );
 }
